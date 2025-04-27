b7cc2aad6d77359b41788b346a1836a0104bae4a
Henry Hale <92443116+henryhale@users.noreply.github.com>
2025-04-27 17:32:25 +0300

    chore: upgrade levels complexity

--- source/model/levels.ts
+++ source/model/levels.ts
@@ -13,24 +13,24 @@ export const LEVELS: Level[] = [
     {
         name: "Sunrise Park",
         rows: 12,
-        cols: 20,
-        locations: 4,
+        cols: 32,
+        locations: 8,
         patterns: [pattern.LINEAR, pattern.REPEAT, pattern.LINEAR, pattern.REPEAT],
         celebrationImage: "img/gameover0.webp"
     },
     {
         name: "Hide And Seek",
         rows: 12,
-        cols: 32,
-        locations: 4,
+        cols: 40,
+        locations: 12,
         patterns: [pattern.LINEAR, pattern.REPEAT, pattern.LINEAR, pattern.REPEAT],
         celebrationImage: "img/gameover1.webp"
     },
     {
         name: "Cold Harbor",
         rows: 12,
-        cols: 40,
-        locations: 8,
+        cols: 48,
+        locations: 12,
         patterns: [pattern.LINEAR, pattern.REPEAT, pattern.LINEAR, pattern.SQUARE],
         celebrationImage: "img/gameover2.webp"
     },
